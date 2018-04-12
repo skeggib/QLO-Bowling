3d39039c3f9a96602174f1547d65996da340c44c
Sebastien Klasa <klasa.sebastien@gmail.com>
2018-04-12 09:59:18 +0200

    Check travis with failing tests

--- bowling.cpp
+++ bowling.cpp
@@ -4,12 +4,12 @@
 
 void bowling::add(std::pair<int, int> p)
 {
-    if (p.first < 0 || p.second < 0)
+    /*if (p.first < 0 || p.second < 0)
         throw std::exception();
     if (p.first + p.second > 10)
         throw std::exception();
     if (_pairs.size() >= 10)
-        throw std::exception();
+        throw std::exception();*/
     _pairs.push_back(p);
 }
 
