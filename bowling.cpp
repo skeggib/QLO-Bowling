b273be0539685387890ecaa2e172dd2b42008b40
Sebastien Klasa <klasa@IS231023>
2018-04-12 11:08:02 +0200

    Add strikes and spares

--- bowling.cpp
+++ bowling.cpp
@@ -4,19 +4,28 @@
 
 void bowling::add(std::pair<int, int> p)
 {
-    /*if (p.first < 0 || p.second < 0)
+    if (p.first < 0 || p.second < 0)
         throw std::exception();
     if (p.first + p.second > 10)
         throw std::exception();
     if (_pairs.size() >= 10)
-        throw std::exception();*/
+        throw std::exception();
     _pairs.push_back(p);
 }
 
 int bowling::score() const
 {
-    int sum = 0;
-    for (auto pair:_pairs)
-        sum += pair.first + pair.second;
+    int sum = 0;        
+    for (int i = 0; i < _pairs.size(); ++i)
+    {
+        sum += _pairs[i].first + _pairs[i].second;
+        if (i > 0)
+        {
+            if (_pairs[i-1].first == 10)
+                sum += _pairs[i].first + _pairs[i].second;
+            else if (_pairs[i-1].first + _pairs[i-1].second == 10)
+                sum += _pairs[i].first;
+        }
+    }
     return sum;
 }
--- bowling_test.cpp
+++ bowling_test.cpp
@@ -19,7 +19,7 @@ TEST_CASE("Bowling API")
         REQUIRE_THROWS(b.add({0, -1}));
     }
 
-    SECTION("max 10 values")
+    SECTION("max 10 pairs")
     {
         bowling b;
         for (int i = 0; i < 10; ++i)
@@ -31,5 +31,53 @@ TEST_CASE("Bowling API")
     {
         bowling b;
         REQUIRE_THROWS(b.add({10, 1}));
+    }
+
+    SECTION("the sum of a tuple is 10")
+    {
+        bowling b;
+        b.add({ 6, 4 });
+
+        SECTION("the next value is between 0 and 10")
+        {
+            b.add({ 5, 5 });
+            REQUIRE(b.score() == 25);
+        }
+        
+        SECTION("the next value is 0")
+        {
+            b.add({ 0, 3 });
+            REQUIRE(b.score() == 13);
+        }
+
+        SECTION("the next value is 10")
+        {
+            b.add({ 10, 0 });
+            REQUIRE(b.score() == 30);
+        }
+    }
+
+    SECTION("the fist value of a tuple is 10")
+    {
+        bowling b;
+        b.add({ 10, 0 });
+
+        SECTION("the sum of the next tuple is 0")
+        {
+            b.add({ 0, 0 });
+            REQUIRE(b.score() == 10);
+        }
+
+        SECTION("the next value is not 10")
+        {
+            b.add({ 3, 2 });
+            REQUIRE(b.score() == 20);
+        }
+
+        SECTION("the next value is 10")
+        {
+            b.add({ 10, 0 });
+            REQUIRE(b.score() == 30);
+        }
     }
 }
