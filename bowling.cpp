a8d12b8711603bb87b5bd58a9ce11aa683685c48
Sebastien Klasa <klasa@IS231023>
2018-05-10 17:37:52 +0200

    Add one bonus pair if the 10th is a spare

--- bowling.cpp
+++ bowling.cpp
@@ -2,14 +2,31 @@
 
 #include <exception>
 
+bool is_strike(std::pair<int, int> p)
+{
+    return p.first == 10;
+}
+
+bool is_spare(std::pair<int, int> p)
+{
+    return !is_strike(p) && p.first + p.second == 10;
+}
+
 void bowling::add(std::pair<int, int> p)
 {
     if (p.first < 0 || p.second < 0)
         throw std::exception();
     if (p.first + p.second > 10)
         throw std::exception();
-    if (_pairs.size() >= 10)
-        throw std::exception();
+
+    if (_pairs.size() >= 9)
+    {
+        if (!is_strike(_pairs[9]) && !is_spare(_pairs[9]) && _pairs.size() >= 10)
+            throw std::exception();
+        if (is_spare(_pairs[9]) && _pairs.size() >= 11)
+            throw std::exception();
+    }
+    
     _pairs.push_back(p);
 }
 
--- bowling_test.cpp
+++ bowling_test.cpp
@@ -31,53 +31,131 @@ TEST_CASE("Bowling API")
     {
         bowling b;
         REQUIRE_THROWS(b.add({10, 1}));
+        REQUIRE_THROWS(b.add({7, 4}));
     }
 
-    SECTION("the sum of a tuple is 10")
+    GIVEN("the sum of a tuple is 10")
     {
         bowling b;
         b.add({ 6, 4 });
 
-        SECTION("the next value is between 0 and 10")
+        WHEN("the next value is between 1 and 9")
         {
             b.add({ 5, 5 });
-            REQUIRE(b.score() == 25);
+            THEN("the value is doubled")
+            {
+                REQUIRE(b.score() == 25);
+            }
         }
         
-        SECTION("the next value is 0")
+        WHEN("the next value is 0")
         {
             b.add({ 0, 3 });
-            REQUIRE(b.score() == 13);
+            THEN("nothing changes")
+            {
+                REQUIRE(b.score() == 13);
+            }
         }
 
-        SECTION("the next value is 10")
+        WHEN("the next value is 10")
         {
             b.add({ 10, 0 });
-            REQUIRE(b.score() == 30);
+            THEN("the 10 is doubled")
+            {
+                REQUIRE(b.score() == 30);
+            }
         }
     }
 
-    SECTION("the fist value of a tuple is 10")
+    GIVEN("the fist value of a tuple is 10")
     {
         bowling b;
         b.add({ 10, 0 });
 
-        SECTION("the sum of the next tuple is 0")
+        WHEN("the sum of the next tuple is 0")
         {
             b.add({ 0, 0 });
-            REQUIRE(b.score() == 10);
+            THEN("nothing changes")
+            {
+                REQUIRE(b.score() == 10);
+            }
         }
 
-        SECTION("the next value is not 10")
+        WHEN("the two next values are not 10")
         {
             b.add({ 3, 2 });
-            REQUIRE(b.score() == 20);
+            THEN("the two values are doubled")
+            {
+                REQUIRE(b.score() == 20);
+            }
         }
 
-        SECTION("the next value is 10")
+        WHEN("the next value is 10")
         {
             b.add({ 10, 0 });
-            REQUIRE(b.score() == 30);
+            b.add({ 3, 2 });
+            THEN("the 10 is doubled and the the next tuple is doubled")
+            {
+                REQUIRE(b.score() == 40);
+            }
+        }
+
+        WHEN("the next tuple is a value between 1 and 9 then a 0")
+        {
+            b.add({ 3, 0 });
+            THEN("the tuple is doubled")
+            {
+                REQUIRE(b.score() == 16);
+            }
+        }
+    }
+
+    GIVEN("the 10th tuple is a spare")
+    {
+        bowling b;
+        for (int i = 0; i < 9; ++i)
+            b.add({ 0, 0 });
+        b.add({ 9, 1 });
+
+        THEN("the 11th tuple is allowed")
+        {
+            REQUIRE_NOTHROW(b.add({ 0, 0 }));
+        }
+
+        WHEN("the 11th tuple is two zeros")
+        {
+            b.add({ 0, 0 });
+            THEN("the 12th tuple is not allowed")
+            {
+                REQUIRE_THROWS(b.add({ 0, 0 }));
+            }
+        }
+
+        WHEN("the 11th tuple is not zero, a spare nor a strike either")
+        {
+            b.add({ 1, 2 });
+            THEN("the 12th tuple is not allowed")
+            {
+                REQUIRE_THROWS(b.add({ 0, 0 }));
+            }
+        }
+
+        WHEN("the 11th tuple is a spare")
+        {
+            b.add({ 9, 1 });
+            THEN("the 12th tuple is not allowed")
+            {
+                REQUIRE_THROWS(b.add({ 0, 0 }));
+            }
+        }
+
+        WHEN("the 11th tuple is a strike")
+        {
+            b.add({ 10, 0 });
+            THEN("the 12th tuple is not allowed")
+            {
+                REQUIRE_THROWS(b.add({ 0, 0 }));
+            }
         }
     }
 }
